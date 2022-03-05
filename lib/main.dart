fa6fa4110ba35d33089d6e2f0a58e330e9a25baf
unknown <shady.samara@gmail.com>
2022-03-05 05:16:08 -0800

    assigment solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:wedad_application/Product.dart';
+import 'package:wedad_application/custom_product_widget.dart';
 import 'package:wedad_application/shady_widget.dart';
 
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:FirstScreen()));
+    home:ListViewTest()));
 }
 class FirstScreen extends StatelessWidget{
-  Map<String,String> userDetails = {
-    'name':'Omar Ahmed Ali',
-    'univ':'Isamic Unversity Of Gaza',
-    'address':'Palestine - Gaza'
-  };
+List<Product> products = [
+  Product('https://sc02.alicdn.com/kf/H9035e8d50a004577ba6da2914d0f93e8q/201725773/H9035e8d50a004577ba6da2914d0f93e8q.jpg', 'Face Cream',500 ),
+    Product('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/29360452568197.5914d0225e4cd.jpg', 'Orange Juise',10 ),
+      Product('https://opentextbc.ca/businessopenstax/wp-content/uploads/sites/246/2018/09/Photo_11.3.jpg', 'Vitamene C',350 ),
+        Product('https://i.pinimg.com/originals/8e/aa/57/8eaa5755758529d6bb8b6ea7110d02e6.jpg', 'Makeup Cream',200 ),
+ Product('https://sc02.alicdn.com/kf/H9035e8d50a004577ba6da2914d0f93e8q/201725773/H9035e8d50a004577ba6da2914d0f93e8q.jpg', 'Face Cream',500 ),
+    Product('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/29360452568197.5914d0225e4cd.jpg', 'Orange Juise',10 ),
+      Product('https://opentextbc.ca/businessopenstax/wp-content/uploads/sites/246/2018/09/Photo_11.3.jpg', 'Vitamene C',350 ),
+        Product('https://i.pinimg.com/originals/8e/aa/57/8eaa5755758529d6bb8b6ea7110d02e6.jpg', 'Makeup Cream',200 ),
+
+ Product('https://sc02.alicdn.com/kf/H9035e8d50a004577ba6da2914d0f93e8q/201725773/H9035e8d50a004577ba6da2914d0f93e8q.jpg', 'Face Cream',500 ),
+    Product('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/29360452568197.5914d0225e4cd.jpg', 'Orange Juise',10 ),
+      Product('https://opentextbc.ca/businessopenstax/wp-content/uploads/sites/246/2018/09/Photo_11.3.jpg', 'Vitamene C',350 ),
+        Product('https://i.pinimg.com/originals/8e/aa/57/8eaa5755758529d6bb8b6ea7110d02e6.jpg', 'Makeup Cream',200 ),
+
+ Product('https://sc02.alicdn.com/kf/H9035e8d50a004577ba6da2914d0f93e8q/201725773/H9035e8d50a004577ba6da2914d0f93e8q.jpg', 'Face Cream',500 ),
+    Product('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/29360452568197.5914d0225e4cd.jpg', 'Orange Juise',10 ),
+      Product('https://opentextbc.ca/businessopenstax/wp-content/uploads/sites/246/2018/09/Photo_11.3.jpg', 'Vitamene C',350 ),
+        Product('https://i.pinimg.com/originals/8e/aa/57/8eaa5755758529d6bb8b6ea7110d02e6.jpg', 'Makeup Cream',200 ),
+
+
+];
   String imageUrl = 'https://images.unsplash.com/photo-1469474968028-56623f02e42e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMDg4MDd8MHwxfHNlYXJjaHwyfHxuYXR1cmV8ZW58MHx8fHwxNjQ2Mjk0NTEx&ixlib=rb-1.2.1&q=80&w=1080';
   getSum(int num1,int y){}
   @override
@@ -38,10 +57,12 @@ bottomNavigationBar: BottomNavigationBar(
     BottomNavigationBarItem(icon: Icon(Icons.favorite),label: 'Favourite')
   ],),
     body: Container(
+      
 
       child: SingleChildScrollView(child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: 
       [
         /*
@@ -55,7 +76,7 @@ bottomNavigationBar: BottomNavigationBar(
           child: Row(children: [
             Container(
               
-              margin: EdgeInsets.symmetric(horizontal: 5),
+              margin: EdgeInsets.symmetric(horizontal: 5,vertical: 10),
               child:  ClipRRect(
               borderRadius:BorderRadius.circular(20) ,
                 child: Image.asset('assets/images/image1.jfif',height: 200,width: 300,fit:BoxFit.cover
@@ -81,10 +102,38 @@ bottomNavigationBar: BottomNavigationBar(
       //     return Center(child: CircularProgressIndicator(),);
       //   }
       // } ,),
-      ShadyWidget(shadyLabel: 'Name: ',shadyDescription: userDetails['name']!),
-      ShadyWidget(shadyLabel:'University: ',shadyDescription:userDetails['univ']!),
-      ShadyWidget(shadyLabel:'Address: ',shadyDescription:userDetails['address']!),
+      Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Text('Products',style: TextStyle(fontSize: 19,fontWeight: FontWeight.w500),)),
+      /////////////////////////////////////
+     ...products.map((product) {
+      return ProductWidget(product);
+    }).toList(),
+      ////////////////////////////////
         ]),
     )),);
+  }
+}
+
+class ListViewTest extends StatelessWidget{
+  List<Product> products = [
+  Product('https://sc02.alicdn.com/kf/H9035e8d50a004577ba6da2914d0f93e8q/201725773/H9035e8d50a004577ba6da2914d0f93e8q.jpg', 'Face Cream',500 ),
+    Product('https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/29360452568197.5914d0225e4cd.jpg', 'Orange Juise',10 ),
+      Product('https://opentextbc.ca/businessopenstax/wp-content/uploads/sites/246/2018/09/Photo_11.3.jpg', 'Vitamene C',350 ),
+        Product('https://i.pinimg.com/originals/8e/aa/57/8eaa5755758529d6bb8b6ea7110d02e6.jpg', 'Makeup Cream',200 ),
+
+
+];
+ 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(appBar: AppBar(title: Text('ListView Test'),),
+    body: ListView.builder(
+      itemCount:products.length,
+      itemBuilder: (context,index){
+        return ProductWidget(products[index]);
+      })
+    );
   }
 }
